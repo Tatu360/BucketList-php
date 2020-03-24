17c44296a308b1a27c7c67ff879b5cf01f78dc18
Tatu360 <52759723+Tatu360@users.noreply.github.com>
2020-03-24 20:25:27 +0300

    Front.php created

---
+++ Front.php
@@ -0,0 +1,19 @@
+#this is the front page of the bucketlist web app
+<html>
+     <head>
+         <title> Front page </title>
+<link rel = "stylesheet" type= "text/css" href= "022.css"/>
+         </head>
+<body>
+
+    <div class="new">
+
+    <h2> Welcome to Buckect List App <br>
+       Do you have an Account?</h2>
+       <div class="log">
+           <a href="Reg.php" class="TK"> Register </a>
+           <a href="app.php" class="TK"> Login </a>
+      </div>
+</body>
+  
+  </html>
