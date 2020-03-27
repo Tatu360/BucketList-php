bd43aaa6ab4905e165d0a49a7ba82cd4454502d3
Tatu360 <52759723+Tatu360@users.noreply.github.com>
2020-03-27 14:14:02 +0300

    fifth commit
    the collection of the bucket list for the user to choose

---
+++ bucketlist.php
@@ -0,0 +1,46 @@
+<html>
+   <head>
+     <title></title>
+          <link rel="stylesheet" type="text/css" href="033.CSS"/> 
+    </head>
+       <body>
+          <h1> Bucket list </h1>
+      <div class="TATU">
+        <form action=""  method="POST">
+       <div id="ROT">
+                <label for="list1"> Get birth to twins</label><br>
+                <input type="checkbox" id="list" name="list2" value="">
+                <label for="list2"> Taking master in computer science(MCS)</label><br>
+                <input type="checkbox" id="list" name="list3" value="">
+                <label for="list3"> Build a House</label><br>
+                <input type="checkbox" id="list" name="list4" value="">
+                <label for="list4"> Read 50 books until the end of the year</label><br>
+                <input type="checkbox" id="list" name="list5" value="">
+                <label for="list5"> Save tshs.50mil</label><br>
+                <input type="checkbox" id="list" name="list6" value="">
+                <label for="list6"> Put myself as first priority</label><br><br>
+     </div>
+             <div id="RET">
+                <input type="checkbox" id="list" name="list1" value="">
+                <label for="list1"> Get birth to twins</label><br>
+                <input type="checkbox" id="list" name="list2" value="">
+                <label for="list2"> Taking master in computer science(MCS)</label><br>
+                <input type="checkbox" id="list" name="list3" value="">
+                <label for="list3"> Build a House</label><br>
+                <input type="checkbox" id="list" name="list4" value="">
+                <label for="list4"> Read 50 books until the end of the year</label><br>
+                <input type="checkbox" id="list" name="list5" value="">
+                <label for="list5"> Save tshs.50mil</label><br>
+                <input type="checkbox" id="list" name="list6" value="">
+                <label for="list6"> Put myself as first priority</label><br><br>
+             </div>
+        <input type="submit"  class="WEQ" value="Submit">
+       </form>
+     </div>
+  </body>
+</html>
+<?php
+if (isset(_POST['submit'])){
+  
+}
+?>
