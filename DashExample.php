a71acb87f864a00f7ba1976efe9874e84751c191
Tatu360 <52759723+Tatu360@users.noreply.github.com>
2020-03-27 14:12:02 +0300

    fourth commit
    application dashboard

---
+++ DashExample.php
@@ -0,0 +1,21 @@
+
+ <html>
+    <head>
+     <link rel="stylesheet" type="text/css" href="011.css"/>
+         <title> Dashboard </title>
+    </head>
+       <body>
+       <div class="pro">
+         <img src="f5.png">
+         <h3> My profile</h3>
+         </div>
+       <div id="sidebar" >
+       
+       <ul>
+          <li> <a href="">Home </a></li>
+          <li> <a href="bucketlist.php"> Collection </a> </li>
+          <li id="re"><a href="out.php"> Logout </a></li>
+       </div>
+
+       </body>
+</html>
